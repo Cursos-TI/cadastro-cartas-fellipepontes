490b49261ba98e9038661712b6cc62aae0e78d89
Fellipe Pontes <ponteeesss@gmail.com>
2026-05-25 19:34:34 -0300

    Refactor main function and update input/output
    Refactor main function and fix print statements.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-int main()
-char estado1;
+int maind (){
+  char estado1;
+  printf ("C");
+  scanf ("%c", &estado1);
 
+  printf ("Voce digitou: %c\n, estado1);
 
-printf ("C:");
-
-scanf (" %c,&estado1");
-
-  printf ("O estado digitado foi: %c\n, estado1");
 
 return 0;
 } 
